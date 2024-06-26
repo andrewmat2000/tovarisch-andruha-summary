a17f76a314ddc7e0068f74e6e79298ce84ee78f7
Andrey Matveev <andrewmat2000@gmail.com>
2024-06-26 15:33:35 +0500

    wip

--- src/frontend/src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  import Link from "@components/Link.svelte";
-  import { _ } from "svelte-i18n";
-</script>
-
-<Link label={$_("main-tasks-link-label")} link="/tasks" />
---
+++ src/frontend/src/routes/code/codewars/+layout.svelte
@@ -0,0 +1,38 @@
+<div>
+  <slot />
+</div>
+
+<style lang="scss">
+  @import url("https://unpkg.com/svelte-highlight/styles/github.css");
+
+  :global(.hljs) {
+    background-color: transparent;
+
+    color: $theme-text-color;
+  }
+
+  :global(.hljs-string, .hljs-literal, .hljs-variable) {
+    color: #2d7cd6;
+  }
+  :global(.hljs-title.function_) {
+    color: #8951f1;
+  }
+  div {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    height: 100%;
+    width: 100%;
+
+    > :global(*:first-child) {
+      border-right: 1px solid $theme-white-color;
+    }
+
+    > :global(*) {
+      padding: 0 15px;
+    }
+  }
+
+  :global(span, h1, h2, div) {
+    color: $theme-text-color;
+  }
+</style>
