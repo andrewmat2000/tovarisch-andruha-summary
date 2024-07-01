b11e5c6830c0cbba33b3f6f7477f8f2d68c72e0a
Andrey Matveev <andrewmat2000@gmail.com>
2024-07-01 18:21:54 +0500

    Added promit test task

--- src/frontend/src/routes/code/+page.svelte
+++ src/frontend/src/routes/code/+page.svelte
@@ -5,3 +5,4 @@ import { _ } from "svelte-i18n"
 </script>
 
 <Link label={$_("codewars")} link="/code/codewars"/>
+<Link label={$_("test-tasks")} link="/code/test-tasks"/>
---
+++ src/frontend/src/routes/code/test-tasks/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Link from "@components/Link.svelte";
+  import { t } from "svelte-i18n";
+  const tasks = [
+    {
+      label: $t("promit-test-task"),
+      link: "/code/test-tasks/promit",
+    },
+  ];
+</script>
+
+<div>
+  {#each tasks as task}
+    <Link label={task.label} link={task.link} />
+  {/each}
+</div>
